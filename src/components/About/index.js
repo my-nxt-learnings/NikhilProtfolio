075a771a26cb611c1653dfca938eb2f9011dac53
Nikhil Galala <mynxtlearnings2025@gmail.com>
2025-04-04 11:13:57 +0530

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,6 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import './index.css'
-import EducationTimeline from '../EducationChrono'
 import FunFactsPopup from '../FunFact'
 
 const certificates = [
